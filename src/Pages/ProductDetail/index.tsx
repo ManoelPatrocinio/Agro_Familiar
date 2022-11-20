346b83bfcd8b6b71cf1904e7daa0ceb87064bea1
ManoelPatrocinio <manoelpatrocinio99@gmail.com>
2022-11-20 01:18:36 -0300

    add  description and reviews on product details

--- src/Pages/ProductDetail/index.tsx
+++ src/Pages/ProductDetail/index.tsx
@@ -9,6 +9,9 @@ import { SectionTitle } from "../../Components/SectionTitle";
 
 export function ProductDetail() {
   const [productQTD, setProductQTD] = useState<number>(1);
+  const [viewProdDetail, setViewProdDetail] = useState<
+    "description" | "reviews"
+  >("description");
 
   const hendleQuantityIncrement = (item: any) => {
     alert("Sorry, ainda em construção");
@@ -16,6 +19,217 @@ export function ProductDetail() {
   const hendleQuantityDecrement = (item: any) => {
     alert("Sorry, ainda em construção");
   };
+
+  const reviews = (
+    <div className="w-full max-h-[25rem] overflow-y-auto  ">
+      <h4 className="w-full text-start  text-md text-palm-700 mb-8">
+        Avaliações do Produto{" "}
+      </h4>
+      <div className="w-full">
+        <div className="flex flex-col md:flex-row justify-start items-start relative mb-6 border-b border-gray-400 pb-4">
+          <div className="w-20 min-w-[5rem] h-20 rounded-[50%] mr-2">
+            <img
+              src="https://icons.iconarchive.com/icons/diversity-avatars/avatars/256/charlie-chaplin-icon.png"
+              className="relative bg-white  h-full w-full"
+              alt="Foto perfil"
+              loading="lazy"
+            />
+          </div>
+          <div className="flex flex-col ">
+            <p className="w-full text-left text-sm  text-gray-800 font-medium font-display">
+              Manoel Patrocino
+            </p>
+            <div className=" flex flex-col md:flex-row justify-start items-start mb-4">
+              <div className="flex mr-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-yellow-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-yellow-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-yellow-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-yellow-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+              </div>
+              <span className="text-xs text-gray-400">
+                20 de abril de 2022, às 14h88
+              </span>
+            </div>
+            <p className="w-full text-sm text-justify md:text-left text-gray-800 pr-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+              quisquam vero adipisci beatae voluptas dolor ame. Lorem ipsum
+              dolor sit amet consectetur adipisicing elit. Maxime quisquam vero
+              adipisci beatae voluptas dolor ame. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Maxime quisquam vero adipisci beatae
+              voluptas dolor ame.
+            </p>
+          </div>
+        </div>
+        <div className="flex  flex-col md:flex-row justify-start items-start relative mb-6 border-b border-gray-400 pb-4">
+          <div className="w-20 min-w-[5rem] h-20 rounded-[50%] mr-2">
+            <img
+              src="https://icons.iconarchive.com/icons/diversity-avatars/avatars/256/charlie-chaplin-icon.png"
+              className="relative bg-white  h-full w-full"
+              alt="Foto perfil"
+              loading="lazy"
+            />
+          </div>
+          <div className="flex flex-col ">
+            <p className="w-full text-left text-sm  text-gray-800 font-medium font-display">
+              Manoel Patrocino
+            </p>
+            <div className=" flex flex-col md:flex-row justify-start items-start mb-4">
+              <div className="flex mr-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-yellow-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-yellow-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-yellow-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-yellow-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+              </div>
+              <span className="text-xs text-gray-400">
+                20 de abril de 2022, às 14h88
+              </span>
+            </div>
+            <p className="w-full text-sm text-justify md:text-left text-gray-800 pr-2 ">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+              quisquam vero adipisci beatae voluptas dolor ame. Lorem ipsum
+              dolor sit amet consectetur adipisicing elit. Maxime quisquam vero
+              adipisci beatae voluptas dolor ame. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Maxime quisquam vero adipisci beatae
+              voluptas dolor ame.
+            </p>
+          </div>
+        </div>
+        <div className="flex  flex-col md:flex-row justify-start items-start relative mb-6 border-b border-gray-400 pb-4">
+          <div className="w-20 min-w-[5rem] h-20 rounded-[50%] mr-2">
+            <img
+              src="https://icons.iconarchive.com/icons/diversity-avatars/avatars/256/charlie-chaplin-icon.png"
+              className="relative bg-white  h-full w-full"
+              alt="Foto perfil"
+              loading="lazy"
+            />
+          </div>
+          <div className="flex flex-col ">
+            <p className="w-full text-left text-sm  text-gray-800 font-medium font-display">
+              Manoel Patrocino
+            </p>
+            <div className=" flex flex-col md:flex-row justify-start items-start mb-4">
+              <div className="flex mr-2">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-yellow-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-yellow-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-yellow-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5 text-yellow-400"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                </svg>
+              </div>
+              <span className="text-xs text-gray-400">
+                20 de abril de 2022, às 14h88
+              </span>
+            </div>
+            <p className="w-full text-sm text-justify md:text-left text-gray-800 pr-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+              quisquam vero adipisci beatae voluptas dolor ame. Lorem ipsum
+              dolor sit amet consectetur adipisicing elit. Maxime quisquam vero
+              adipisci beatae voluptas dolor ame. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Maxime quisquam vero adipisci beatae
+              voluptas dolor ame.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+  const description = (
+    <div className="w-full">
+      <h4 className="w-full text-start  text-md text-palm-700 mb-8">
+        Descrição do Produto{" "}
+      </h4>
+      <p className="w-full text-justify text-sm text-gray-800">
+        Produto fresco e bem cuidado, retirado assim que a compra é confirmada.
+        plantado na roça de seu Zé Borges Com disponibilidade de entrega a cada
+        semana, a partir de terça-feira Conseguimos atender até uma demanda de
+        200 maços por semana A depender da quantidade podemos entregar em sua
+        casa se morar na mesma cidade.
+      </p>
+    </div>
+  );
 
   return (
     <>
@@ -47,8 +261,8 @@ export function ProductDetail() {
         </div>
         <div className="w-full md:w-1/2 pt-8 md:pt-0 md:pl-8 md:pr-20">
           <div className="w-full flex flex-col items-center md:items-start ">
-            <h2 className="w-full text-center md:text-start text-palm-700 text-md md:text-xl font-medium">
-              Maço de ruculo{" "}
+            <h2 className="w-full text-center md:text-start text-palm-700 text-lg md:text-xl font-medium mb-1 ">
+              Maço de ruculo
             </h2>
             <div className="flex w-1/4 items-center justify-center md:justify-start mb-6">
               <span className="text-sm text-gray-400"> 4.6 </span>
@@ -70,11 +284,11 @@ export function ProductDetail() {
             <p className="w-full text-justify md:text-start text-xs md:text-sm text-gray-400 pt-4 md:pt-16 pb-4">
               <span className="text-red-500"> Aviso:</span> Todo o processo de
               negociação e entrega , deve ser feito entre o cliente e
-              responsavel por este produto.{" "}
+              responsavel por este produto.
             </p>
 
             <div className="w-full flex flex-col md:flex-row  items-center mt-2 md:mt-0 mb-4">
-              <div className="w-[75%] md:w-1/4 flex justify-start rounded border border-palm-700 py-1 mb-2 md:mb-0 md:mr-8 ">
+              <div className="w-[75%] md:w-1/4 flex justify-start rounded border border-palm-700 py-1 mb-4 md:mb-0 md:mr-8 ">
                 <button
                   className="w-1/4 h-full text-center text-xl text-palm-700"
                   type="button"
@@ -95,7 +309,7 @@ export function ProductDetail() {
               </div>
               <button
                 type="button"
-                className="w-[75%] flex items-center justify-center px-2 py-2.5 md:px-0 md:py-2.5 bg-palm-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-palm-500 hover:shadow-lg focus:bg-palm-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-palm-500 active:shadow-lg transition duration-150 ease-in-out"
+                className="w-full flex items-center justify-center px-2 py-3.5 md:px-0 md:py-2.5 bg-palm-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-palm-500 hover:shadow-lg focus:bg-palm-500 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-palm-500 active:shadow-lg transition duration-150 ease-in-out"
               >
                 <IconAddList w={"20"} h={"20"} color="#fff" className="mr-1" />
                 Adicionar a lista
@@ -111,6 +325,28 @@ export function ProductDetail() {
           </div>
         </div>
       </main>
+      <section className="w-full flex flex-col items-start justify-start px-8 md:px-20 mt-8">
+        <header className="w-full flex items-center font-medium justify-evenly py-3 text-center text-lg text-palm-700   underline-offset-4 mb-8">
+          <button
+            className=" focus:underline "
+            onClick={() => {
+              setViewProdDetail("description");
+            }}
+          >
+            {" "}
+            Descrição
+          </button>
+          <button
+            className=" focus:underline "
+            onClick={() => {
+              setViewProdDetail("reviews");
+            }}
+          >
+            Avaliações
+          </button>
+        </header>
+        {viewProdDetail === "description" ? description : reviews}
+      </section>
     </>
   );
 }
